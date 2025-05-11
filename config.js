f4ea43255f15f09caf56ffd535671611cd1d75df
frxsh286 <zokoumd286@gmail.com>
2025-05-11 11:11:43 -0400

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || 'HansTz&MWdmFLIZ#pwqAIHDb-MN518Aa19e8mrvObpfzjHtq8wAptx-ceiFZ8',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "fraisha-MD",
+    OWNER_NAME: process.env.OWNER_NAME || "Hantz-MD",
     NUMERO_OWNER : process.env.NUMERO_OWNER || " 263717087382",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
